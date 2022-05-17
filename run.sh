5d5df39ddd5397ed9facaea8890c35751b073b01
João Boing <joao@conectra.com.br>
2022-05-17 08:19:47 -0300

    feat: update s3 folder structure

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ DATE=`date +%Y-%m-%d_%H-%M`
 
 YEAR=`date +%Y`
 MONTH=`date +%m`
+DAY=`date +%d`
 
 BKPNAME=${DB_NAME}-${DATE}
 
@@ -13,4 +14,4 @@ echo $(date) "Backup iniciado"
 mongodump --uri=${MONGO_URL} --gzip --archive=/tmp/${BKPNAME}.agz
 
 echo $(date) "Copiando arquivo para o bucket s3"
-aws s3 cp /tmp/${BKPNAME}.agz s3://${BUCKET_NAME}/${BKP_PREFIX}${DB_NAME}/${YEAR}/${MONTH}/${BKPNAME}.agz --region ${AWS_REGION}
+aws s3 cp /tmp/${BKPNAME}.agz s3://${BUCKET_NAME}/${YEAR}/${MONTH}/${DAY}/${BKPNAME}.agz --region ${AWS_REGION}
